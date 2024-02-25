bd3121a5192af65dc93c78c6b4f2c79bdfb93f85
sunmoonlion <13701819268@163.com>
2024-02-25 21:38:10 +0800

    feat：路由方案1vite-plutin-pages

---
+++ src/pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<!-- <script setup lang="ts">
+//重定向方案2：编程式导航
+import { useRouter } from 'vue-router'
+const router = useRouter()
+router.push('/')
+</script>
+
+<style scoped>
+
+</style> -->
+<!-- 重定向方案3：pages官方方案
+<route lang="yaml">
+    name: home
+    redirect: 
+        path: /
+</route> -->
+
+<!-- 重定向方案4：pages官方方案json格式 -->
+<route>
+    {name:'home',redirect:'/'}
+</route>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import routes from '~pages'
+// import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'home',
+  //     component: HomeView
+  //   },
+  //   {
+  //     path: '/about',
+  //     name: 'about',
+  //     // route level code-splitting
+  //     // this generates a separate chunk (About.[hash].js) for this route
+  //     // which is lazy-loaded when the route is visited.
+  //     component: () => import('../views/AboutView.vue')
+  //   }
+  // ]
+  routes
 })
-
+//重定向方案1：导航守卫
+router.beforeEach((to, from, next) => {
+  //判断路由
+  if (to.path === '/home') {
+    next('/')
+  }
+  next()
+})
 export default router

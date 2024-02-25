ddb63d130b888d3e614fc32a8f2d4e09a6d5458b
sunmoonlion <13701819268@163.com>
2024-02-26 00:17:33 +0800

    feat:路由方案2/unplugin-vue-router

---
+++ src/pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <div > 
+        <RouterLink to="/about">about</RouterLink>
+        <RouterLink to="/">home</RouterLink>      
+        <h1>This is an about page</h1>
+    </div>
+</template>
+<script setup lang="ts">
+import { RouterLink } from 'vue-router/auto';
+</script>
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+import { createWebHistory } from 'vue-router';
+<template>
+    <div>
+        hello world by unplugin-vue-router
+    </div>
+    <RouterLink to="/about">about</RouterLink>
+    <RouterLink to="/">home</RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router/auto';
+</script>
+
+<style scoped>
+
+</style>

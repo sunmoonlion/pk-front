67e197c9a20152ee64c175058e8591c261c972ff
sunmoonlion <13701819268@163.com>
2024-03-02 16:48:20 +0800

    add AutoImport and vueuse

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,24 @@
 import { createWebHistory } from 'vue-router';
 <template>
-    <div>
+    <div ref="target">
         hello world by unplugin-vue-router
     </div>
     <RouterLink to="/about">about</RouterLink>
     <RouterLink to="/">home</RouterLink>
     <h1 class="text-3xl font-bold underline">hello world</h1>
+    <div>{{ msg }}</div>
+    <div>x:{{ x }},y:{{ y }},isOutside:{{ isOutside }}</div>
 </template>
 
 <script setup lang="ts">
 import { RouterLink } from 'vue-router/auto';
+
+
+const msg = ref("hello world by unplugin-auto-import");
+
+const target = ref(null);
+
+const {x,y,isOutside} = useMouseInElement(target)
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import VueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,6 +16,24 @@ export default defineConfig({
     vue(),
     vueJsx(),
     UnoCSS(),
+    AutoImport({ 
+      include: [
+    /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+    /\.vue$/,
+    /\.vue\?vue/, // .vue
+    /\.md$/, // .md
+  ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        // 'vue-router',
+        VueRouterAutoImports,
+        '@vueuse/core',
+      ],
+     
+    }),
   ],
   resolve: {
     alias: {

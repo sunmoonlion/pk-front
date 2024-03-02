a6d6a8029fe1f1a657c85c6396f118664ed59055
sunmoonlion <13701819268@163.com>
2024-03-02 17:51:08 +0800

    add ElementPlus

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,14 @@ import { createWebHistory } from 'vue-router';
     <div>x:{{ x }},y:{{ y }},isOutside:{{ isOutside }}</div>
     <HelloWorld></HelloWorld> 
     <userUserComponent></userUserComponent>
+    <el-row class="mb-4">
+        <el-button>Default</el-button>
+        <el-button type="primary">Primary</el-button>
+        <el-button type="success">Success</el-button>
+        <el-button type="info">Info</el-button>
+        <el-button type="warning">Warning</el-button>
+        <el-button type="danger">Danger</el-button>
+    </el-row>
 </template>
 
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -38,6 +39,7 @@ export default defineConfig({
     Components({
       directoryAsNamespace: true,
       collapseSamePrefixes: true,
+      resolvers: [ElementPlusResolver()],
     }),
   ],
   resolve: {
